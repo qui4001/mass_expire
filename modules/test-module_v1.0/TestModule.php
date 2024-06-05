981fa901155f0f0cc20341bc785b0bbdd33ba003
qui4001 <qui4001@med.cornell.edu>
2024-06-05 00:05:48 -0500

    JS script is using passed php variable

--- modules/test-module_v1.0/TestModule.php
+++ modules/test-module_v1.0/TestModule.php
@@ -2,6 +2,8 @@
 
 // error handling try/catch during query execution
 // error handling when printing out the resut of fetch
+// add a clause when no username is passed during fetch/open
+// remove deprecated when detecing dom chnage
 
 // Set the namespace defined in your config file
 namespace WCM\TestModule;
@@ -18,44 +20,40 @@ class TestModule extends \ExternalModules\AbstractExternalModule
 
         ?>
         <script>
-                document.addEventListener('DOMNodeInserted', () => {
-                    if(document.getElementById('view_user_div') && document.getElementById('view_user_div').childElementCount > 1 && !document.getElementById('mass_expire')){
-                        me_button = document.createElement('button');
-                        me_button.id = 'mass_expire';
-                        me_button.textContent = 'Mass Expire';
-                        document.getElementById('indv_user_info').rows[15].cells[1].append(me_button);
+            var module = <?=$this->getJavascriptModuleObjectName()?>;
+            var base_url = module.tt('greeting');
+            base_url = new DOMParser().parseFromString(base_url, "text/html");
+            base_url = base_url.documentElement.textContent;
 
-                        me_button.addEventListener('click', function(){
-                            user_details = document.getElementById('indv_user_info');
-                            username = user_details.rows[2].cells[1];
-                            username = trim(username.innerHTML);
-                            url = 'http://localhost/redcap_v14.0.14/ExternalModules/?prefix=test-module&page=test&username='+username;
-                            
-                            // open(url, target="_blank", "popup=yes,left=100,top=100,width=480,height=320");
-                            fetch(url) .then(response => response.text()) .then(result => { const data = JSON.parse(result); alert('Newly expired project ' + data.unexpired + ' and already expired ' + data.already);});
-                        });
-                    }
-                });
+            document.addEventListener('DOMNodeInserted', () => {
+                if(document.getElementById('view_user_div') 
+                        && document.getElementById('view_user_div').childElementCount > 1 
+                        && !document.getElementById('mass_expire')){
+
+                    me_button = document.createElement('button');
+                    me_button.id = 'mass_expire';
+                    me_button.textContent = 'Mass Expire';
+                    document.getElementById('indv_user_info').rows[15].cells[1].append(me_button);
+
+                    me_button.addEventListener('click', function(){
+                        user_details = document.getElementById('indv_user_info');
+                        username = user_details.rows[2].cells[1];
+                        username = trim(username.innerHTML);
+                        query_url = base_url + '&username=' + username;
+                        
+                        // open(query_url, target="_blank", "popup=yes,left=100,top=100,width=480,height=320");
+
+                        fetch(query_url)
+                            .then(response => response.text())
+                            .then(result => { 
+                                const data = JSON.parse(result); 
+                                alert('Newly expired project ' + data.unexpired + ' and already expired ' + data.already);
+                            });
+                    });
+                }
+            });
         </script>
         <?php
-
-        /*
-        $em_url = $em_url . "&username=test1";
-        echo $em_url;
-
-        $ch = curl_init();
-        // curl_setopt($ch, CURLOPT_URL, $em_url);
-        curl_setopt($ch, CURLOPT_URL, 'http://localhost/redcap_v14.0.14/ExternalModules/?prefix=test-module&page=test&username=test1');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-        $response = curl_exec($ch);
-        echo $response;
-
-        curl_close($ch);
-
-        redirectAfterHook($em_url, true);
-        */
 
     }
 }
