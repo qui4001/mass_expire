c35bf633c7bc7ea9071aacdc749e0f0f8804f3d0
qui4001 <qui4001@med.cornell.edu>
2024-06-04 23:40:46 -0500

    JSON returning two values and alert putting them in a sentence

--- modules/test-module_v1.0/test.php
+++ modules/test-module_v1.0/test.php
@@ -9,14 +9,16 @@ $username = $_REQUEST["username"];
 // $response = ['username' => $username];
 // echo json_encode($response); // {"a":1,"b":2,"c":3,"d":4,"e":5}
 
-
 $today = date("Y-m-d");
-$query = "select * from redcap_user_rights where username = '$username' and (expiration is null or expiration > '$today')";
 
-$result = mysqli_query($conn, $query);
+$already_expired_query = "select * from redcap_user_rights where username = '$username' and not (expiration is null or expiration > '$today')";
+$already_expired_result = mysqli_query($conn, $already_expired_query);
+
+$unexpired_query = "select * from redcap_user_rights where username = '$username' and (expiration is null or expiration > '$today')";
+$unexpired_result = mysqli_query($conn, $unexpired_query);
 
 
-while ($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($unexpired_result)) {
     $project_id = $row['project_id'];
     $temp_query = "update redcap_user_rights set expiration = current_date() where username = '$username' and project_id = $project_id;"; 
 
@@ -28,4 +30,4 @@ while ($row = mysqli_fetch_assoc($result)) {
     }
 }
 
-echo json_encode(['value' => $result->num_rows]);
+echo json_encode(['unexpired' => $unexpired_result->num_rows, 'already' => $already_expired_result->num_rows]);
