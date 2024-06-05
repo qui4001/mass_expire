3526e14bcc095093a6a2c411829485e6af16f2e5
qui4001 <qui4001@med.cornell.edu>
2024-06-04 20:37:18 -0500

    mass_expire only plugin, TestModule ready for demo

--- modules/test-module_v1.0/TestModule.php
+++ modules/test-module_v1.0/TestModule.php
@@ -15,23 +15,26 @@ class TestModule extends \ExternalModules\AbstractExternalModule
     function redcap_control_center()
     {
         ?>
+
             <script type='text/javascript'>
                 document.addEventListener('DOMNodeInserted', () => {
                     if(document.getElementById('view_user_div') && document.getElementById('view_user_div').childElementCount > 1 && !document.getElementById('mass_expire')){
                         me_button = document.createElement('button');
                         me_button.id = 'mass_expire';
                         me_button.textContent = 'Mass Expire';
-                        document.getElementById('view_user_div').appendChild(me_button);
+                        document.getElementById('indv_user_info').rows[15].cells[1].append(me_button);
 
                         me_button.addEventListener('click', function(){
-                            // fetch('https://api.github.com/repos/octocat/Spoon-Knife/issues')
-                            fetch('../../plugins/mass_expire.php?username=test1')
-                                .then(response => response.text())
-                                .then(result => alert('Expired user from all projects.'));
+                            user_details = document.getElementById('indv_user_info');
+                            username = user_details.rows[2].cells[1];
+                            username = trim(username.innerHTML);
+                            open('../../plugins/mass_expire.php?username='+username, target="_blank", "popup=yes,left=100,top=100,width=480,height=320");
+                            // fetch('../../plugins/mass_expire.php?username='+username) .then(response => response.text()) .then(result => alert('Expired user from all projects.'));
                         });
                     }
                 });
             </script>
+
         <?php
 
     }
