b59ffb63e249948d9ea182da95532953f3e0c46b
Quazi Irfan <qui4001@med.cornell.edu>
2024-09-04 08:44:31 -0500

    Using jquery dialog

--- MassExpire.php
+++ MassExpire.php
@@ -1,73 +1,107 @@
 <?php
 
-// Next step: show WCM BA for modifications; QA, Code review; OPT Release REDCap EM as v.1 and continue working on v.2
-// need to check if it is conflicting with native functions(check if suspension, deletion, reaction of user work when EM is enabled)
-// error handling try/catch during query execution
-// error handling when printing out the resut of fetch
-// add a clause when no username is passed during fetch/open
-// remove deprecated when detecing dom chnage
-// Build EM's internal log for troubleshooting
-// username wasn't declared as var/let when assigning variable the first time -- investigate
-// run built in script checker
-// REDCap dialog box instead of HTML dialog
-// Present it to EM Meeting
-// Would it be an issue that I am working locally?
-// find how we deploy, folder name represents version number
-// investigate ORM https://stackoverflow.com/questions/129677/how-can-i-sanitize-user-input-with-php
+// EM framework version
 
-// Set the namespace defined in your config file
+// $module->query vs db_query
+// https://github.com/vanderbilt-redcap/external-module-framework-docs/blob/main/methods/querying.md
+
+// $module->getUser() returns empty - Why?
+// could we use $module->isAuthenticated() instead?
+
 namespace WeillCornellMedicine\MassExpire;
 
-// Declare your module class, which must extend AbstractExternalModule 
 class MassExpire extends \ExternalModules\AbstractExternalModule
 {
     function redcap_control_center()
     {
-
         $this->initializeJavascriptModuleObject();
         $em_url = $this->escape($this->getUrl("expire_user.php"));
         $this->tt_addToJavascriptModuleObject("fullurl",$em_url);
 
         ?>
         <script>
-            var module = <?=$this->getJavascriptModuleObjectName()?>;
-            var base_url = module.tt('fullurl');
+            let module = <?=$this->getJavascriptModuleObjectName()?>;
+            let base_url = module.tt('fullurl');
             base_url = new DOMParser().parseFromString(base_url, "text/html");
             base_url = base_url.documentElement.textContent;
 
-            document.addEventListener('DOMNodeInserted', () => {
-                if(document.getElementById('user-search') 
-                        && document.getElementById('view_user_div').childElementCount > 1 
-                        && !document.getElementById('mass_expire')){
+            let observer = new MutationObserver(callback);
 
-                    me_button = document.createElement('button');
-                    me_button.id = 'mass_expire';
-                    me_button.textContent = 'Mass Expire';
+            function callback (mutations) {
+                for(const m of mutations){
+                    if(m.type === 'childList' && 
+                        document.getElementById('user-search') && document.getElementById('view_user_div').childElementCount > 1 && 
+                        !document.getElementById('mass_expire'))
+                    {
+                        me_button = document.createElement('button');
+                        me_button.id = 'mass_expire';
+                        me_button.textContent = 'Mass Expire';
 
-                    var user_details = document.getElementById('indv_user_info');
-                    user_details.rows[15].cells[1].append(me_button);
+                        let user_details = document.getElementById('indv_user_info');
+                        user_details.rows[16].cells[1].append(me_button);
 
-                    me_button.addEventListener('click', function(){
-                        username = user_details.rows[2].cells[1];
-                        username = trim(username.innerHTML);
-                        query_url = base_url + '&username=' + username;
+                        me_button.addEventListener('click', function(){
+                            username = user_details.rows[2].cells[1];
+                            username = trim(username.innerHTML);
+                            query_url = base_url + '&username=' + username;
+                            
+                            showProgress(true);
+                            async function handleMassExpire(){ 
+                                try{
+                                    const mass_expire_http = await fetch(query_url);
+                                    const mass_expire_json = await mass_expire_http.text();
+                                    const data = JSON.parse(mass_expire_json);
+                                    
+                                    let dialog_message = '';
+                                    if(data.status === 'failure')
+                                        dialog_message = data.description;
+                                    else
+                                        dialog_message = 'Projects expired with yesterday\'s date: <b>' + data.unexpired + '</b><br> Projects already expired (no action taken): <b>' + data.already +'</b>';
 
-                        fetch(query_url)
-                            .then(response => response.text())
-                            .then(result => { 
-                                simpleDialog(
-                                    "Content of the dialog " + result, 
-                                    "dialog title", null, null, 
-                                    "console.log('onCloseJs')", "closeBtnTxt",
-                                    "simpleDialog('Expired from 10 projects', 'title', null, 200)", "okBtnTxt"
-                                );
-                                // const data = JSON.parse(result); 
-                                // alert('Projects expired with today\'s date: ' + data.unexpired + 
-                                        // '\nProjects already expired (no action taken): ' + data.already );
-                            });
-                    });
+                                    showProgress(false);
+                                    simpleDialog(
+                                        dialog_message,
+                                        "Mass Expire", null, null, 
+                                        null, "Close",
+                                        null, null
+                                    );
+                                } catch(e){
+                                    console.log(e);
+
+                                    showProgress(false);
+                                    simpleDialog(
+                                        'Internal Error - <b> ' + e + '</b>', 
+                                        "Mass Expire", null, null, 
+                                        null, "Close",
+                                        null, null
+                                    );
+                                }
+                            }
+
+                            handleMassExpire();
+                            // fetch(query_url)
+                            //     .then(response => response.text())
+                            //     .then(result => {
+                            //         showProgress(false);
+                            //         const data = JSON.parse(result); 
+                            //         simpleDialog(
+                            //             'Projects expired with yesterday\'s date: <b>' + data.unexpired + '</b><br> Projects already expired (no action taken): <b>' + data.already +'</b>', 
+                            //             "Mass Expire Result", null, null, 
+                            //             null, "Close",
+                            //             null, null
+                            //         );
+                            // });
+                        });
+                    }
                 }
-            });
+            }
+
+            observerOptions = {
+                childList: true,
+                subtree: true
+            }
+
+            observer.observe(document.body, observerOptions);
         </script>
         <?php
 
--- expire_user.php
+++ expire_user.php
@@ -1,36 +1,61 @@
 <?php
+namespace ExternalModules;
 
 require_once "../../redcap_connect.php";
 
 header('Content-type: application/json');
 http_response_code(200);
 
-$username = $_REQUEST["username"];
-
-$today = date("Y-m-d");
-
-$already_expired = $module->query(
-    "select * from redcap_user_rights where username = ? and not (expiration is null or expiration > ?)", 
-    [$username, $today]
-);
-
-$unexpired_query = $module->query(
-    "select * from redcap_user_rights where username = ? and (expiration is null or expiration > ?)",
-    [$username, $today]
-);
-
-while ($row = $unexpired_query->fetch_assoc()) {
-    $project_id = $row['project_id'];
-    $temp_query = "update redcap_user_rights set expiration = current_date() - INTERVAL 1 DAY where username = '$username' and project_id = $project_id;"; 
-    $yesterday = date("Y-m-d", strtotime("-1 day"));
-    
-    if(db_query($temp_query)){
-        \REDCap::logEvent("Updated User Expiration " . $username, "user = '" . $username. "'", $temp_query, NULL, NULL, $project_id);
-        $module->log('Expired user \''.$username.'\' from project ID '.$project_id.' on '.$yesterday);
+if(ExternalModules::isSuperUser()){
+    $username = $_REQUEST["username"];
+    if(!isset($username)){
+        $error_json = json_encode(['status'=>'failure', 'description'=>'Critial error: Endpoint was called without mandatory an username.']);
+        echo $error_json;
+        exit();
     }
+
+    $num_projects = $module->query(
+        "select count(*) as proj_count from redcap_user_rights where username = ?",
+        [$username]
+    );
+
+    $num_projects_result = $num_projects->fetch_assoc();
+
+    if(!$num_projects_result['proj_count']){
+        $error_json = json_encode(['status'=>'failure', 'description'=>'User <b>'. $username . '</b> is not associated with any project.']);
+        echo $error_json;
+        exit();
+    }
+
+    $today = date("Y-m-d");
+
+    $already_expired = $module->query(
+        "select * from redcap_user_rights where username = ? and not (expiration is null or expiration > ?)", 
+        [$username, $today]
+    );
+
+    $unexpired_query = $module->query(
+        "select * from redcap_user_rights where username = ? and (expiration is null or expiration > ?)",
+        [$username, $today]
+    );
+
+    while ($row = $unexpired_query->fetch_assoc()) {
+        $project_id = $row['project_id'];
+        $temp_query = "update redcap_user_rights set expiration = current_date() - INTERVAL 1 DAY where username = '$username' and project_id = $project_id;"; 
+        $yesterday = date("Y-m-d", strtotime("-1 day"));
+        
+        if(db_query($temp_query)){
+            \REDCap::logEvent("Updated User Expiration " . $username, "user = '" . $username. "'", $temp_query, NULL, NULL, $project_id);
+            $module->log('Expired user \''.$username.'\' from project ID '.$project_id.' on '.$yesterday);
+        }
+    }
+
+    $return_json = json_encode(['status'=>'success','unexpired' => $unexpired_query->num_rows, 'already' => $already_expired->num_rows]); 
+    // for ($x = 0; $x <= 100000000; $x++) {}
+    echo $return_json;
 }
-
-$return_json = json_encode(['unexpired' => $unexpired_query->num_rows, 'already' => $already_expired->num_rows]); 
-
-echo $return_json;
+else{
+    $error_json = json_encode(['status'=>'failure', 'description'=>'User is not a superuser.']);
+    echo $error_json;
+}
 ?>
