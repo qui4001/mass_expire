a7241f8ad3c523386f634c2322204d4990df45df
Quazi Irfan <qui4001@med.cornell.edu>
2024-09-25 06:07:22 -0500

    Update is using module->query; using immediately invoked function

--- MassExpire.php
+++ MassExpire.php
@@ -1,12 +1,12 @@
 <?php
 
 // EM framework version
-
-// $module->query vs db_query
-// https://github.com/vanderbilt-redcap/external-module-framework-docs/blob/main/methods/querying.md
+// https://github.com/vanderbilt-redcap/external-module-framework-docs/blob/main/versions/README.md
 
 // $module->getUser() returns empty - Why?
-// could we use $module->isAuthenticated() instead?
+// check if only one row was returned after update
+// call free to close all open transactions
+// perhaps uses getQueryLogsSql($sql) instead of rebuilding the query string?
 
 namespace WeillCornellMedicine\MassExpire;
 
@@ -15,93 +15,84 @@ class MassExpire extends \ExternalModules\AbstractExternalModule
     function redcap_control_center()
     {
         $this->initializeJavascriptModuleObject();
-        $em_url = $this->escape($this->getUrl("expire_user.php"));
-        $this->tt_addToJavascriptModuleObject("fullurl",$em_url);
+        $mass_expire_em_url = $this->escape($this->getUrl("expire_user.php"));
+        $this->tt_addToJavascriptModuleObject("fullurl",$mass_expire_em_url);
 
         ?>
-        <script>
-            let module = <?=$this->getJavascriptModuleObjectName()?>;
-            let base_url = module.tt('fullurl');
-            base_url = new DOMParser().parseFromString(base_url, "text/html");
-            base_url = base_url.documentElement.textContent;
+        <script type="text/javascript">
+            (function(){
+                let module = <?=$this->getJavascriptModuleObjectName()?>;
+                let base_url = module.tt('fullurl');
+                base_url = new DOMParser().parseFromString(base_url, "text/html");
+                base_url = base_url.documentElement.textContent;
 
-            let observer = new MutationObserver(callback);
+                let observer = new MutationObserver(callback);
 
-            function callback (mutations) {
-                for(const m of mutations){
-                    if(m.type === 'childList' && 
-                        document.getElementById('user-search') && document.getElementById('view_user_div').childElementCount > 1 && 
-                        !document.getElementById('mass_expire'))
-                    {
-                        me_button = document.createElement('button');
-                        me_button.id = 'mass_expire';
-                        me_button.textContent = 'Mass Expire';
+                function callback (mutations) {
+                    for(const m of mutations){
+                        if(m.type === 'childList' &&
+                            document.getElementById('user-search') &&
+                            document.getElementById('view_user_div').childElementCount > 1 &&
+                            !document.getElementById('mass_expire'))
+                        {
+                            me_button = document.createElement('button');
+                            me_button.id = 'mass_expire';
+                            me_button.textContent = 'Mass Expire';
 
-                        let user_details = document.getElementById('indv_user_info');
-                        user_details.rows[16].cells[1].append(me_button);
+                            let user_details = document.getElementById('indv_user_info');
+                            user_details.rows[16].cells[1].append(me_button);
 
-                        me_button.addEventListener('click', function(){
-                            username = user_details.rows[2].cells[1];
-                            username = trim(username.innerHTML);
-                            query_url = base_url + '&username=' + username;
-                            
-                            showProgress(true);
-                            async function handleMassExpire(){ 
-                                try{
-                                    const mass_expire_http = await fetch(query_url);
-                                    const mass_expire_json = await mass_expire_http.text();
-                                    const data = JSON.parse(mass_expire_json);
-                                    
-                                    let dialog_message = '';
-                                    if(data.status === 'failure')
-                                        dialog_message = data.description;
-                                    else
-                                        dialog_message = 'Projects expired with yesterday\'s date: <b>' + data.unexpired + '</b><br> Projects already expired (no action taken): <b>' + data.already +'</b>';
+                            me_button.addEventListener('click', function(){
+                                username = user_details.rows[2].cells[1];
+                                username = trim(username.innerHTML);
+                                query_url = base_url + '&username=' + username;
 
-                                    showProgress(false);
-                                    simpleDialog(
-                                        dialog_message,
-                                        "Mass Expire", null, null, 
-                                        null, "Close",
-                                        null, null
-                                    );
-                                } catch(e){
-                                    console.log(e);
+                                showProgress(true);
+                                async function handleMassExpire(){
+                                    try{
+                                        const mass_expire_http = await fetch(query_url);
+                                        const mass_expire_json = await mass_expire_http.text();
+                                        const data = JSON.parse(mass_expire_json);
 
-                                    showProgress(false);
-                                    simpleDialog(
-                                        'Internal Error - <b> ' + e + '</b>', 
-                                        "Mass Expire", null, null, 
-                                        null, "Close",
-                                        null, null
-                                    );
+                                        let dialog_message = '';
+                                        if(data.status === 'failure')
+                                            dialog_message = data.description;
+                                        else
+                                            dialog_message = 'Projects expired with yesterday\'s date: <b>' + data.unexpired + '</b><br> Projects already expired (no action taken): <b>' + data.already +'</b>';
+
+                                        showProgress(false);
+                                        simpleDialog(
+                                            dialog_message,
+                                            "Mass Expire", null, null,
+                                            null, "Close",
+                                            null, null
+                                        );
+                                    } catch(e){
+                                        console.log(e);
+
+                                        showProgress(false);
+                                        simpleDialog(
+                                            'Internal Error - <b> ' + e + '</b>',
+                                            "Mass Expire", null, null,
+                                            null, "Close",
+                                            null, null
+                                        );
+                                    }
                                 }
-                            }
 
-                            handleMassExpire();
-                            // fetch(query_url)
-                            //     .then(response => response.text())
-                            //     .then(result => {
-                            //         showProgress(false);
-                            //         const data = JSON.parse(result); 
-                            //         simpleDialog(
-                            //             'Projects expired with yesterday\'s date: <b>' + data.unexpired + '</b><br> Projects already expired (no action taken): <b>' + data.already +'</b>', 
-                            //             "Mass Expire Result", null, null, 
-                            //             null, "Close",
-                            //             null, null
-                            //         );
-                            // });
-                        });
+                                handleMassExpire();
+                            });
+                        }
                     }
                 }
-            }
 
-            observerOptions = {
-                childList: true,
-                subtree: true
-            }
+                observerOptions = {
+                    childList: true,
+                    subtree: true
+                }
 
-            observer.observe(document.body, observerOptions);
+                observer.observe(document.body, observerOptions);
+            })();
         </script>
         <?php
 
--- expire_user.php
+++ expire_user.php
@@ -9,7 +9,7 @@ http_response_code(200);
 if(ExternalModules::isSuperUser()){
     $username = $_REQUEST["username"];
     if(!isset($username)){
-        $error_json = json_encode(['status'=>'failure', 'description'=>'Critial error: Endpoint was called without mandatory an username.']);
+        $error_json = json_encode(['status'=>'failure', 'description'=>'Critical error: External module endpoint was called without an username.']);
         echo $error_json;
         exit();
     }
@@ -41,11 +41,15 @@ if(ExternalModules::isSuperUser()){
 
     while ($row = $unexpired_query->fetch_assoc()) {
         $project_id = $row['project_id'];
-        $temp_query = "update redcap_user_rights set expiration = current_date() - INTERVAL 1 DAY where username = '$username' and project_id = $project_id;"; 
-        $yesterday = date("Y-m-d", strtotime("-1 day"));
-        
-        if(db_query($temp_query)){
+        $update_query = $module->query(
+            "update redcap_user_rights set expiration = current_date() - INTERVAL 1 DAY where username = ? and project_id = ?",
+            [$username, $project_id]
+        );
+
+        if($update_query){
+            $temp_query = "update redcap_user_rights set expiration = current_date() - INTERVAL 1 DAY where username = '$username' and project_id = $project_id;";
             \REDCap::logEvent("Updated User Expiration " . $username, "user = '" . $username. "'", $temp_query, NULL, NULL, $project_id);
+            $yesterday = date("Y-m-d", strtotime("-1 day"));
             $module->log('Expired user \''.$username.'\' from project ID '.$project_id.' on '.$yesterday);
         }
     }
